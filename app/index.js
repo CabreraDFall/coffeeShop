e83a066d7a5230ac19fa9a249a703d6d4d032892
CabreraDFall <testingdfall@gmail.com>
2024-08-22 10:05:39 +0200

    ShoppingIconWithBadge

--- app/index.js
+++ app/index.js
@@ -1,14 +1,21 @@
 import { Text, View } from "react-native";
 import Layout from "../compontents/Layout";
-import { StatusBar } from "expo-status-bar";
 
+import ShoppingIcon from "../compontents/ShoppingIcon";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { theme } from "../theme";
 export default function index() {
+  const userName = "Fedrerick";
+
   return (
     <>
       <Layout
         children={
-          <View className="flex-1">
-            <Text className="text-white text-2xl">xx</Text>
+          <View className="flex-1 flex-row justify-between bet mt-8 px-5 align-middle">
+            <Text className="text-white text-lg font-light">
+              Hola, <Text className="font-medium">{userName}</Text>
+            </Text>
+            <ShoppingIcon />
           </View>
         }
       />
---
+++ compontents/ShoppingIcon.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { theme } from "../theme";
+
+const ShoppingIconWithBadge = () => {
+  const badgeCount = 1;
+  return (
+    <View style={styles.iconContainer}>
+      <MaterialIcons
+        name="shopping-bag"
+        size={theme.sizeIcons.extraLarge} // Puedes usar theme.sizeIcons.extraLarge si lo tienes definido
+        color={theme.whiteBase} // Puedes usar theme.whiteBase si lo tienes definido
+      />
+      {badgeCount > 0 && (
+        <View style={styles.badgeContainer}>
+          <Text style={styles.badgeText}>{badgeCount}</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    position: "relative",
+  },
+  badgeContainer: {
+    position: "absolute",
+    right: -3,
+
+    backgroundColor: theme.orangeBase,
+    borderRadius: 10,
+    width: 15,
+    height: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  badgeText: {
+    color: "white",
+    fontSize: 10,
+    fontWeight: "bold",
+  },
+});
+
+export default ShoppingIconWithBadge;
